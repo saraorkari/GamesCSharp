166ea7b3c97c272d7b4e48843d0be1acd555e3f9
sara <saraor1412@gmail.com>
2022-12-11 20:28:31 +0200

    Games

---
+++ Reversy/Reversy/Reversy/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reversy
+{
+    class Program
+    {
+        static void PrintColor(int[,] mat)
+        {
+            int c1 = 0, c2 = 0;
+            Console.Clear();
+            Console.WriteLine("  1  2  3  4  5  6  7  8  9  0");
+            for (int i = 1; i < mat.GetLength(0) - 1; i++)
+            {
+                if (i == 10)
+                    Console.Write("0 ");
+                else
+                    Console.Write(i + " ");
+
+                for (int j = 1; j < mat.GetLength(1) - 1; j++)
+                {
+                    if (mat[i, j] == 1)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        c1++;
+                    }
+                    else
+                    if (mat[i, j] == 2)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        c2++;
+                    }
+                    else
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    Console.Write("__|");
+                }
+                Console.WriteLine();
+            }
+            Console.BackgroundColor = ConsoleColor.Black;
+            if ((c1 + c2) == ((mat.GetLength(0) - 2) * (mat.GetLength(1) - 2)) - 4)
+            {
+                if (c1 > c2)
+                {
+                    Console.WriteLine("The player 1 is winner!!!");
+                }
+                else
+                {
+                    Console.WriteLine("The player 2 is winner!!!");
+                }
+            }
+        }
+        static bool Check(int[,] mat, int i, int j)
+        {
+            bool flag = false;
+            int[,] m = { { 1, 1, 1 }, { 1, 0, 1 }, { 1, 1, 1 } };
+            if (i < 1 || i > 10 || j < 1 || j > 10)
+            {
+                Console.WriteLine("Error! play again");
+                return true;
+            }
+            if (mat[i, j] != 0)
+            {
+                Console.WriteLine("Error! play again");
+                return true;
+            }
+            for (int a = -1; a < 2 && !flag; a++)
+            {
+                for (int b = -1; b < 2 && !flag; b++)
+                {
+                    if (mat[i + a, j + b] != 0)
+                    {
+                        flag = true;
+                    }
+                }
+            }
+            if (!flag)
+            {
+                Console.WriteLine("Error! play again");
+                return true;
+            }
+            return false;
+        }
+        static void Play(int[,] mat, int i, int j)
+        {
+            int t, s;
+            //מימין
+            for (t = j - 1; t >= 1 && mat[i, t] != mat[i, j] && mat[i, t] != 0; t--)
+                if (mat[i, t - 1] == mat[i, j])
+                    for (int r = t; r < j; r++)
+                        mat[i, r] = mat[i, j];
+            //משמאל
+            for (t = j + 1; t < mat.GetLength(0) && mat[i, t] != mat[i, j] && mat[i, t] != 0; t++)
+                if (mat[i, t + 1] == mat[i, j])
+                    for (int r = t; r >= j; r--)
+                        mat[i, r] = mat[i, j];
+            //מלמעלה
+            for (t = i - 1; i >= 1 && mat[t, j] != mat[i, j] && mat[t, j] != 0; t--)
+                if (mat[t - 1, t] == mat[i, j])
+                    for (int r = t; r < i; r++)
+                        mat[r, j] = mat[i, j];
+            //מלמטה
+            for (t = i + 1; t < mat.GetLength(0) && mat[t, j] != mat[i, j] && mat[t, j] != 0; t++)
+                if (mat[t + 1, j] == mat[i, j])
+                    for (int r = t; r >= i; r--)
+                        mat[r, j] = mat[i, j];
+            //אלכסון משמאל למעלה
+            for (t = i - 1, s = j - 1; t >= 1 && s >= 1 && mat[t, s] != mat[i, j] && mat[t, s] != 0; t--, s--)
+                if (mat[t - 1, s - 1] == mat[i, j])
+                    for (int r = t, q = s; r < i && q < j; r++, q++)
+                        mat[r, q] = mat[i, j];
+            //אלכסון מימין למטה
+            for (t = i + 1, s = j + 1; t < mat.GetLength(0) && s < mat.GetLength(1) && mat[t, s] != mat[i, j] && mat[t, s] != 0; t++, s++)
+                if (mat[t - 1, s - 1] == mat[i, j])
+                    for (int r = t, q = s; r >= i && q >= j; r--, q--)
+                        mat[r, q] = mat[i, j];
+            //אלכסון מימין למעלה
+            for (t = i - 1, s = j + 1; t >= 1 && s < mat.GetLength(0) && mat[t, s] != mat[i, j] && mat[t, s] != 0; t--, s++)
+                if (mat[t - 1, s + 1] == mat[i, j])
+                    for (int r = t, q = s; r < i && q >= j; r++, q--)
+                        mat[r, q] = mat[i, j];
+            //אלכסון משמאל למטה
+            for (t = i + 1, s = j - 1; t < mat.GetLength(0) && s >= 1 && mat[t, s] != mat[i, j] && mat[t, s] != 0; t++, s--)
+                if (mat[t + 1, s - 1] == mat[i, j])
+                    for (int r = t, q = s; r >= i && q < j; r--, q++)
+                        mat[r, q] = mat[i, j];
+        }
+        static void Main(string[] args)
+        {
+            //Reversy
+            const int n = 10;
+            int[,] mat = new int[n + 2, n + 2];
+            mat[mat.GetLength(0) / 2 - 1, mat.GetLength(0) / 2 - 1] = mat[mat.GetLength(0) / 2, mat.GetLength(0) / 2] = 1;
+            mat[mat.GetLength(0) / 2, mat.GetLength(0) / 2 - 1] = mat[mat.GetLength(0) / 2 - 1, mat.GetLength(0) / 2] = 2;
+            PrintColor(mat);
+            int i, j, p;
+            for (p = 1; p <= (n * n) - 4; p++)
+            {
+                if (p % 2 != 0)
+                {
+                    Console.WriteLine("Player 1, your time is now");
+                    Console.WriteLine("enter place between 1-10");
+                    i = int.Parse(Console.ReadLine());
+                    j = int.Parse(Console.ReadLine());
+                    while (Check(mat, i, j))
+                    {
+                        Console.WriteLine("Player 1, your time is now");
+                        Console.WriteLine("enter place between 1-10");
+                        i = int.Parse(Console.ReadLine());
+                        j = int.Parse(Console.ReadLine());
+                    }
+                    mat[i, j] = 1;
+                }
+                else
+                {
+                    Console.WriteLine("Player 2, your time is now");
+                    Console.WriteLine("enter place between 1-10");
+                    i = int.Parse(Console.ReadLine());
+                    j = int.Parse(Console.ReadLine());
+                    while (Check(mat, i, j))
+                    {
+                        Console.WriteLine("Player 2, your time is now");
+                        Console.WriteLine("enter place between 1-10");
+                        i = int.Parse(Console.ReadLine());
+                        j = int.Parse(Console.ReadLine());
+                    }
+                    mat[i, j] = 2;
+                }
+                Play(mat, i, j);
+                PrintColor(mat);
+            }
+        }
+    }
+}
---
+++ איש תלוי 2/איש תלוי 2/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace איש_תלוי_2
+{
+    public partial class Form1 : Form
+    {
+        string[] strs = { "hello world", "good morning", "how are you?", "good luck!", "very nice" };
+        int x;
+        Panel p, p1;
+        string select;
+        public Form1()
+        {
+            InitializeComponent();
+            //מקלדת//
+            p = new Panel();
+            p.Size = new Size(280, 150);
+            p.Location = new Point(40, 250);
+            Controls.Add(p);
+            p1 = new Panel();
+            p1.Size = new Size(500, 50);
+            p1.Location = new Point(200, 50);
+            Controls.Add(p1);
+            int x, y, j;
+            x = 0;
+            y = 0;
+            j = 1;
+            for (int i = 0; i < 26; i++)
+            {
+                j++;
+                Button b = new Button();
+                b.Text = Convert.ToChar('a' + i).ToString();
+                b.Size = new Size(25, 25);
+                b.Location = new Point(x + j * 30, y);
+                b.Enabled = false;
+                b.Click += new EventHandler(button2_Click);
+                p.Controls.Add(b);
+                if ((i + 1) % 7 == 0)
+                {
+                    x = 0;
+                    y += 40;
+                    j = 1;
+                }
+            }
+            Button b1 = new Button();
+            Button b2 = new Button();
+            b1.Text = ('?').ToString();
+            b2.Text = ('!').ToString();
+            b1.Size = new Size(25, 25);
+            b2.Size = new Size(25, 25);
+            b1.Location = new Point(x + 7 * 30, y);
+            b2.Location = new Point(x + 8 * 30, y);
+            b1.Enabled = false;
+            b2.Enabled = false;
+            b1.Click += new EventHandler(button2_Click);
+            b2.Click += new EventHandler(button2_Click);
+            p.Controls.Add(b1);
+            p.Controls.Add(b2);
+            //התחל משחק//
+            Button b3 = new Button();
+            b3.Text = "start game";
+            b3.Size = new Size(90, 30);
+            b3.Location = new Point(700, 10);
+            b3.Click += new EventHandler(button1_Click);
+            Controls.Add(b3);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            p1.Controls.Clear();
+            x = 0;
+            Random r = new Random();
+            int o = r.Next(strs.Length);
+            select = strs[o];
+            Label l = new Label();
+            for (int i = 0; i < select.Length; i++)
+            {
+                l.Size = new Size(30, 30);
+                l.Location = new Point(i * 40, 0);
+                if (select[i] == ' ')
+                {
+                    l.Text = " ";
+                }
+                else
+                {
+                    l.Text = "__";
+                }
+                p1.Controls.Add(l);
+                l = new Label();
+            }
+            //הפעלת המקלדת//
+            for (int i = 0; i < p.Controls.Count; i++)
+            {
+                p.Controls[i].Enabled = true;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int index = -1;
+            bool flag = false;
+            (sender as Button).Enabled = false;
+            for (int i = 0; i < select.Length; i++)
+            {
+                index = select.IndexOf((sender as Button).Text, index + 1);
+                if (index > -1)
+                {
+                    p1.Controls[index].Text = (sender as Button).Text;
+                    flag = true;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if (!flag)
+            {
+                x++;
+            }
+            if (!IsContinue())
+            {
+                MessageBox.Show("Victory!!!");
+            }
+            if (x > 6)
+            {
+                MessageBox.Show("Lost:-( Try again");
+            }
+        }
+
+        public bool IsContinue()
+        {
+            for (int i = 0; i < p1.Controls.Count; i++)
+            {
+                if (p1.Controls[i].Text == "__")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
